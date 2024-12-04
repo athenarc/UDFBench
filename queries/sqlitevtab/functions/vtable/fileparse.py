b3132cd4a56584aab117bc5669a843edf68b67d9
johnfouf <johnfouf@gmail.com>
2024-12-04 12:35:04 +0200

    clean up

--- queries/sqlitevtab/functions/vtable/fileparse.py
+++ queries/sqlitevtab/functions/vtable/fileparse.py
@@ -1,44 +1,3 @@
-"""
-
-.. function:: rowidvt(query:None)
-
-Returns the query input result adding rowid number of the result row.
-
-:Returned table schema:
-    Same as input query schema with addition of rowid column.
-
-    - *rowid* int
-        Input *query* result rowid.    
-
-Examples::
-
-    >>> table1('''
-    ... James   10	2
-    ... Mark    7	3
-    ... Lila    74	1
-    ... ''')
-    >>> sql("rowidvt select * from table1")
-    rowid | a     | b  | c
-    ----------------------
-    1     | James | 10 | 2
-    2     | Mark  | 7  | 3
-    3     | Lila  | 74 | 1
-    >>> sql("rowidvt select * from table1 order by c")
-    rowid | a     | b  | c
-    ----------------------
-    1     | Lila  | 74 | 1
-    2     | James | 10 | 2
-    3     | Mark  | 7  | 3
-
-    Note the difference with rowid table column.
-
-    >>> sql("select rowid,* from table1 order by c")
-    rowid | a     | b  | c
-    ----------------------
-    3     | Lila  | 74 | 1
-    1     | James | 10 | 2
-    2     | Mark  | 7  | 3
-"""
 from . import setpath
 from . import vtbase
 import functions
@@ -48,10 +7,7 @@ import json
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-
-### Classic stream iterator
 registered=True
-
 class fileparse(vtbase.VT):
   def VTiter(self, *parsedArgs, **envars):
     largs, dictargs = self.full_parse(parsedArgs)
@@ -148,25 +104,7 @@ class fileparse(vtbase.VT):
     else:
         raise ValueError(f"Unsupported file format: {file_type}")
 
-
-
-
 def Source():
     return vtbase.VTGenerator(fileparse)
-
-if not ('.' in __name__):
-    """
-    This is needed to be able to test the function, put it at the end of every
-    new function you create
-    """
-    import sys
-    from . import setpath
-    from functions import *
-    testfunction()
-    if __name__ == "__main__":
-        reload(sys)
-        sys.setdefaultencoding('utf-8')
-        import doctest
-        doctest.testmod()
 
 
